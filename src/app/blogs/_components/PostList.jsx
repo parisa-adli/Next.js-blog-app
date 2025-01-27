e58cd6ce8bf3ad270ddc5789a6882127f682606b
parisa-adli <parisa.adli1996@gmail.com>
2025-01-27 10:49:57 +0330

    Next/Image component config localhost

--- src/app/blogs/_components/PostList.jsx
+++ src/app/blogs/_components/PostList.jsx
@@ -1,11 +1,21 @@
+import Image from "next/image";
+
 async function PostList() {
   await new Promise((res) => setTimeout(() => res(), 3000));
 
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/post/list`);
   const {
     data: { posts },
-    } = await res.json();
-    
-  return posts.length > 0 ? posts.map((post) => <div>{post.title}</div>) : null;
+  } = await res.json();
+
+  return posts.length > 0 ? (
+    <div className="grid grid-cols-12 gap-8">
+      {posts.map((post) => (
+        <div className="col-span-12 sm:col-span-6 lg:col-span-4 border border-secondary-100 p-2 rounded-lg">
+          <Image src={post.coverImageUrl} alt="" width={400} height={400} />
+        </div>
+      ))}
+    </div>
+  ) : null;
 }
 export default PostList;
