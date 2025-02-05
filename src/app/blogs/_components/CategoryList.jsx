308e976df537dc2f0e6f7478b3f630668438e6ff
parisa-adli <parisa.adli1996@gmail.com>
2025-02-05 11:30:00 +0330

    post content-author-avatar

--- src/app/blogs/_components/CategoryList.jsx
+++ src/app/blogs/_components/CategoryList.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 async function CategoryList() {
-  await new Promise((res) => setTimeout(() => res(), 3000));
+  // await new Promise((res) => setTimeout(() => res(), 3000));
 
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/category/list`);
   const {
@@ -10,18 +10,18 @@ async function CategoryList() {
   //   console.log(categories);
 
   return (
-    <ui className="space-y-4">
-      <Link href="/blogs">همه</Link>
+    <ul className="space-y-4">
+      <Link href={`/blogs/`}>همه</Link>
       {categories.map((category) => {
         return (
-          <li key={category.id} className="list-none">
+          <li key={category._id} className="list-none">
             <Link href={`/blogs/category/${category.slug}`}>
               {category.title}
             </Link>
           </li>
         );
       })}
-    </ui>
+    </ul>
   );
 }
 export default CategoryList;
--- src/app/blogs/_components/PostList.jsx
+++ src/app/blogs/_components/PostList.jsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import CoverImage from "./CoverImage";
+import Link from "next/link";
+import { ClockIcon } from "@heroicons/react/24/outline";
+import Avatar from "@/ui/Avatar";
+import Author from "./Author";
 
 async function PostList() {
-  await new Promise((res) => setTimeout(() => res(), 3000));
+  // await new Promise((res) => setTimeout(() => res(), 3000));
 
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/post/list`);
   const {
@@ -11,14 +16,28 @@ async function PostList() {
   return posts.length > 0 ? (
     <div className="grid grid-cols-12 gap-8">
       {posts.map((post) => (
-        <div className="col-span-12 sm:col-span-6 lg:col-span-4 border border-secondary-100 p-2 rounded-lg">
-          <div className="relative aspect-video overflow-hidden rounded-md mb-6">
-            <Image
-              src={post.coverImageUrl}
-              alt=""
-              fill
-              className="object-cover object-center hover:scale-110 transition-all duration-300 ease-out"
-            />
+        <div
+          key={post._id}
+          className="col-span-12 sm:col-span-6 lg:col-span-4 border border-secondary-100 p-2 rounded-lg"
+        >
+          <CoverImage {...post} />
+          {/* post content */}
+          <div>
+            <Link href={`/blogs/${post.slug}`}>
+              <h2 className="mb-4 font-bold text-secondary-700">
+                {post.title}
+              </h2>
+            </Link>
+            {/* post author - readingTime */}
+            <div className="flex items-center justify-between">
+              <Author {...post.author} />
+              <div className="flex items-center text-[10px] text-secondary-500">
+                <ClockIcon className="w-4 h-4 stroke-secondary-500 ml-1" />
+                <span className="ml-1">خواندن: </span>
+                <span className="ml-1">{post.readingTime}</span>
+                <span>دقیقه</span>
+              </div>
+            </div>
           </div>
         </div>
       ))}
