4cc42f5da3b9052749648a29514ed06c157a2510
parisa-adli <parisa.adli1996@gmail.com>
2025-02-23 13:31:18 +0330

    create category page

---
+++ src/app/blogs/(postList)/category/[categorySlug]/page.jsx
@@ -0,0 +1,26 @@
+import PostList from "app/blogs/_components/PostList";
+
+async function Category({ params }) {
+  const { categorySlug } = params;
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/post/list?categorySlug=${categorySlug}`
+  );
+
+  const { data } = await res.json();
+  const { posts } = data || {};
+
+  console.log(posts);
+
+  return (
+    <div>
+      {posts.length === 0 ? (
+        <p className="text-lg text-secondary-600">
+          پستی در این دسته بندی پیدا نشد
+        </p>
+      ) : (
+        <PostList />
+      )}
+    </div>
+  );
+}
+export default Category;
