52e1a3e38ec53ab1778baebf6203268634178f6c
parisa-adli <parisa.adli1996@gmail.com>
2025-03-05 20:55:52 +0330

    search post

--- src/app/(dashboard)/profile/posts/page.jsx
+++ src/app/(dashboard)/profile/posts/page.jsx
@@ -1,12 +1,21 @@
 import { Suspense } from "react";
 import PostsTable from "./_/components/PostsTable";
 import Fallback from "@/ui/FallBack";
+import Search from "@/ui/Search";
+import { CreatePost } from "./_/components/Buttons";
+import queryString from "query-string";
 
-function page() {
+async function page({ searchParams }) {
+  const query = queryString.stringify(searchParams);
   return (
     <div>
-      <Suspense fallback={<Fallback />}>
-        <PostsTable />
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 text-secondary-700 mb-12 items-center">
+        <h1 className="font-bold text-xl">لیست پست ها</h1>
+        <Search />
+        <CreatePost />
+      </div>
+      <Suspense fallback={<Fallback />} key={query}>
+        <PostsTable query={query} />
       </Suspense>
     </div>
   );
