a0cb90debc3794c75266d791f495eead66dd3513
parisa-adli <parisa.adli1996@gmail.com>
2025-03-05 15:24:39 +0330

    create drawer

--- src/app/(dashboard)/profile/_components/SideBar.jsx
+++ src/app/(dashboard)/profile/_components/SideBar.jsx
@@ -3,12 +3,14 @@
 import {
   ArrowLeftStartOnRectangleIcon,
   HomeIcon,
+  XMarkIcon,
 } from "@heroicons/react/24/outline";
 import SideBarNavs from "./SideBarNavs";
 import Link from "next/link";
+import ButtonIcon from "@/ui/ButtonIcon";
 
-function SideBar() {
-//   const { logout } = useAuth();
+function SideBar({ onClose }) {
+  //   const { logout } = useAuth();
 
   //   const logoutHandler = async () => {
   //     await logout;
@@ -16,21 +18,31 @@ function SideBar() {
 
   return (
     <div className="overflow-y-auto flex flex-col p-5 h-screen pt-10 lg:pt-8">
-      {/* Drawer header */}
-      <Link
-        href="/"
-        className="flex items-center gap-x-4 justify-center text-secondary-700 border-b  border-b-secondary-200 
-        pb-2 mb-6"
-      >
-        <HomeIcon className="w-6 h-6" />
-        <span> نکست بلاگ</span>
-      </Link>
-      {/* Drawer content */}
+      {/* SideBar header */}
+      <div className="flex items-center justify-between w-full mb-5 pb-2 border-b border-b-secondary-200">
+        <Link
+          href="/"
+          className="flex items-center gap-x-4 justify-center text-secondary-700 lg:flex-1"
+        >
+          <HomeIcon className="w-6 h-6" />
+          <span> نکست بلاگ</span>
+        </Link>
+
+        <ButtonIcon
+          onClick={onClose}
+          className="block lg:hidden border-none"
+          variant="outline"
+        >
+          <XMarkIcon />
+        </ButtonIcon>
+      </div>
+
+      {/* SideBar content */}
       <div className="overflow-y-auto flex-auto">
         <SideBarNavs />
         <div
           //   onClick={logoutHandler}
-          className="flex items-center gap-x-2 rounded-2xl font-medium transition-all duration-200 text-secondary-700 py-3 px-4 hover:text-red-400 cursor-pointer"
+          className="flex items-center gap-x-1 rounded-2xl font-medium transition-all duration-200 text-secondary-700 py-3 px-4 hover:text-red-400 cursor-pointer"
         >
           <ArrowLeftStartOnRectangleIcon className="ml-4 h-5 w-5" />
           <span>خروج</span>
